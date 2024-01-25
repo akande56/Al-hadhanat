63d2e24c99f6566a88b65eac8857fb51ad63d256
akande56 <abdulsalamabubakar52@gmail.com>
2024-01-25 07:43:14 +0100

    update3

--- build.sh
+++ build.sh
@@ -6,4 +6,4 @@ pip install -r requirements/production.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
-echo "from django.contrib.auth import get_user_model; User= get_user_model(); User.objects.create_superuser('admin@gmail.com', 'admin' , 'admin123.')"| python manage.py shell
+echo "from django.contrib.auth import get_user_model; User= get_user_model(); User.objects.create_superuser('admin', 'admin@gmail.com' , 'admin123.')"| python manage.py shell
